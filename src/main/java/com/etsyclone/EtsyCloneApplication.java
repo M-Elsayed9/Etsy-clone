7789dd99f2ba8dace734e0e0dde21dc46dc81a1d
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-02-14 01:51:24 -0500

    Project initilization

---
+++ src/main/java/com/etsyclone/EtsyCloneApplication.java
@@ -0,0 +1,13 @@
+package com.etsyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtsyCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtsyCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etsyclone/EtsyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etsyclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtsyCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
